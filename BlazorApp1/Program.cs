c2e439aae08831509ed05d8c839af0ddec0c722f
Thibeault De Mulder <thibeaultdm@hotmail.com>
2024-10-21 16:24:53 +0200

    Still not interactive

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,16 +1,6 @@
-using BlazorUI.Components;
-using BlazorUI.Interfaces;
-using BlazorUI.ViewModels;
-using BlazorUI.Models;
-using Flurl.Http;
-
+using BlazorApp1.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddScoped(sp => new FlurlClient { BaseUrl = "http://localhost:7089/Orchestration/" });
-builder.Services.AddTransient<IDataModel, DataModel>();
-builder.Services.AddTransient<ICustomerViewModel, CustomerViewModel>();
-builder.Services.AddTransient<IAddInvoiceViewModel, AddInvoiceViewModel>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
