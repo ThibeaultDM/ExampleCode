2439075e7f58e228188204402ca0dd2d285380e8
Thibeault De Mulder <thibeaultdm@hotmail.com>
2024-10-30 12:23:36 +0100

    comment

--- NewInvoiceCommunicationLayer/Controllers/NewInvoiceController.cs
+++ NewInvoiceCommunicationLayer/Controllers/NewInvoiceController.cs
@@ -197,6 +197,7 @@ namespace NewInvoiceCommunicationLayer.Controllers
                     case "Unrecognized Guid format":
                         message = $"{EnumDescription.GetDescription(InvoiceExceptionTypes.NotGuid)}";
                         break;
+                    // Unknown Errors
                     default:
                         message = ex.Message;
                         break;
